c926ef66241e71de21970913210ab865b834e85a
Ersel Uğraşır <eugrasir@gmail.com>
2016-03-08 23:48:35 +0200

    added graph

--- ArraySearch/index.php
+++ ArraySearch/index.php
@@ -1,5 +1,11 @@
 <?php
 
+/** 
+ 10,100,1.000,10.000,100.000 elemanlı diziler 100'er kere çalıştırılıp dizide bulunan elemanların
+ ortalama kaç denemede bulunduğu gösterilmiştir($total / 100). Eğer seçilen eleman dizide yoksa max. deneme sayısı
+ göz önüne alınmıştır. İlgili grafik binarySearchGraph.jpg olarak kaydedilmiştir.
+ **/
+
 /**
  $try bir elamanın kaçıncı seferde bulunduğu
  $elemanSayisi dizinin kaç elemanlı olacağı
@@ -18,25 +24,32 @@ $maxNumber = $elemanSayisi;
 
 $myArray = createArray($elemanSayisi);
 
+$total = 0;
+
 for ($i=0; $i < 100; $i++) { 
 
-	$arananSayi = rand(1,$maxNumber + 10 * $i);
+	$arananSayi = rand(1,$maxNumber);
 
 	// $result değeri için kullanilabilir fonksiyonlar : 'linearSearch' ve 'binarySearch'
 	$result = binarySearch($myArray,$arananSayi);
 
 	if ($result == -1) {
 
-	echo "----- Dizide ".$arananSayi." diye bir sayı bulamadım.";
+		echo "----- Dizide ".$arananSayi." diye bir sayı bulamadım.";
 
 	} else {
 
 		echo "+++".$arananSayi." sayısını ".$try.". denemede buldum.";
+		
 	}
 
+	$total += $try;
+	
 	$try = 0;
 
 	echo "<br />";
+
+	if($i == 99){ echo $total / 100; }
 }
 
 function binarySearch($myArray,$number){
@@ -46,6 +59,7 @@ function binarySearch($myArray,$number){
 
     while ($left <= $right) {
         $mid = ($left + $right) / 2;
+        
 		$GLOBALS["try"]++;
 
         if ($myArray[$mid] == $number) {
